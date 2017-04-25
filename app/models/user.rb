188282a403e870282f7c163f345a036bcf759283
chaithanya padi <chaithanya@theinnerhour.com>
2017-04-25 20:11:59 +0530

    added snippets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable,
+  :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  
+  has_many :snippets, dependent: :destroy
 end
